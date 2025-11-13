f3609786d476cad7f4b0fc7f648f4bc637a2de9b
Omid Lak Mazaheri <omidlmazaheri@gmail.com>
2025-11-13 12:46:01 +0330

    Add files via upload

---
+++ jsProject/script.js
@@ -0,0 +1,9 @@
+// OMID: Array
+let names=["Omid","Ali","Mohammad"];
+names[2]="nader";
+
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index]) ;
+    
+}
+console.log("end point")
